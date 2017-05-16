e903babf333baade81c7b04c5e798c8a79e652b5
Derek Rosenzweig <drosenzweig@pet360.com>
2017-05-16 17:38:35 -0400

    Fix dependencies and include dist folder for direct use in git

--- src/.wrapper.js
+++ src/.wrapper.js
@@ -1,6 +1,6 @@
 (function(root, factory) {
     if (typeof define == 'function' && define.amd) {
-        define(['jquery', 'doT', 'jQuery.extendext'], factory);
+        define(['jquery', 'dot/doT', 'jquery-extendext'], factory);
     }
     else {
         factory(root.jQuery, root.doT);
